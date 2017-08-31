bec8e923c4b830e08066381d4aa423a8b39d4ef6
Tarciso <tarcisoloiola@hotmail.com>
2017-08-30 21:26:43 -0400

    moving characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,60 @@
-var pickAvatar = false;
-var pickEnemy = false;
+function attack(){
+
+	$(".myEnemy").empty();
+	$("button").text("Pick another enemy");
+	pickEnemy = false;
+
+
+
+	if(counter == 3){
+		$("button").attr("style", "display:none");
+		alert("We Have Our Savier??");
+	}
+	console.log("Attack Porra");
+}
+
+
+
+var pickAvatar = this.pickAvatar; 
+var pickEnemy = this.pickEnemy; 
+var counter = this.counter;
+
+pickAvatar = false;
+pickEnemy = false;
+counter = 0;
+
 
 
 $(".avatars").on("click", function(){
 	var imgClicked = $("<img>");
+	var a = $(".avatars").attr("id");
 	imgClicked = $(this);
+	a = $(this.id);
 	imgClicked.addClass("avatars");
 	imgClicked.attr("src","imgClicked");
+	$("button").attr("style", "display: initial !important; margin-left: 20px; margin-right: 20px");
 
 if(pickAvatar == false && pickEnemy == false){
 	pickAvatar = true;
-	$(".myAvatar").html(imgClicked);
-	console.log(pickAvatar);
-	console.log(pickEnemy);
+	var myImgAvatar = imgClicked;
+	$(".myAvatar").html(myImgAvatar);	
 }
 else if(pickAvatar  == true && pickEnemy == false){
 		pickEnemy = true;
-		$(".myEnemy").html(imgClicked);
+		counter++;
+		var myImgEnemy = imgClicked;
+		$(".myEnemy").html(myImgEnemy);
+
+		$("button").text("Attack");
+
+		// console.log(a.selector);
 }
 
 
-
+if(pickAvatar == true && pickEnemy == true){
+	$("button").on("click", function(){
+		attack();
+	});
+}
 
 });
